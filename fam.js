468479a31e72fe46bf8eab2c5fc4e3cefcc732f9
Anusha <anushaj.01395@gmail.com>
2026-04-28 10:27:02 +0530

    updated all

--- fam.js
+++ fam.js
@@ -1,3 +1,4 @@
 console.log("I have completed msc computer science");
 console.log("he like to read newspaper every morning");
-console.log("she love to cooking")
+console.log("she love to cooking");
+console.log("he like play outdoor game");
